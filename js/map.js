aef9938b7cae1ad385052a1ed5ffa33104947a49
davidxvuong <david.vuong@me.com>
2015-03-08 12:54:31 -0400

    Updated code to display nearby stores (if any) that is in walking distance from the destination

--- js/map.js
+++ js/map.js
@@ -2,6 +2,8 @@ var map;
 var initLatLng;
 var finalLatLng;
 var travelMethod;
+var infoWindow;
+var nearby_places;
 
 function initialize() {
 	var myLatlng = new google.maps.LatLng(53.854234, -97.318359);
@@ -13,11 +15,13 @@ function initialize() {
     }
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 	
-	var start = markerFactory(initLatLng, map, "Start");
-	var end = markerFactory(finalLatLng, map, "Destination");
 	var travel;
 	var directionServices = new google.maps.DirectionsService();
 	var directionDisplay = new google.maps.DirectionsRenderer();
+	
+	infoWindow = new google.maps.InfoWindow();
+	nearby_places = new google.maps.places.PlacesService(map);
+	google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
 	
 	directionDisplay.setMap(map);
 	directionDisplay.setPanel(document.getElementById('directions'));
@@ -55,14 +59,51 @@ function initialize() {
 	});
 }
 
-function markerFactory(latLngObj, mapObj, text) {
-	var marker = new google.maps.Marker({
-		position: latLngObj,
-		map: mapObj,
-		title: text,
-	});
+function performSearch() {
+	var request = {
+		location: finalLatLng,
+		radius: '500',
+		types: ['store']
+	};
 	
-	return marker;
+	nearby_places.radarSearch(request, function(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0, result; i < 20; i++) {
+				result = results[i];
+				console.log(result);
+				markerFactory(result);
+			}
+		}
+		else {
+			console.log(status);
+		}
+	});
+}
+
+function markerFactory(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: {
+      // Star
+      path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      scale: 1/4,
+      strokeColor: '#bd8d2c',
+      strokeWeight: 1
+    }
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    nearby_places.getDetails(place, function(result, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        infoWindow.setContent(result.name);
+		infoWindow.open(map, marker);
+      }
+      
+    });
+  });
 }
 
 window.onload = function load() {
