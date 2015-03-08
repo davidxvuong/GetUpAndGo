4df0a5561fc6a83b80fb1c60b13a1971ddfcc417
hj3yoo <hj3yoo@uwaterloo.ca>
2015-03-08 00:30:06 -0500

    sessionStorage implemented

--- js/interface.js
+++ js/interface.js
@@ -1,10 +1,12 @@
-EARTH_RADIUS = 6371;
+var EARTH_RADIUS = 6371;
+var initLat = 0;
+var initLong = 0;
 
 function getPoint(initLat, initLong, radius) {
-	maxDeg = radtoDeg(radius / EARTH_RADIUS);
-	randLat = (Math.random() * 2 - 1) * maxDeg;
-	maxLong = Math.sqrt(Math.pow(maxDeg,2) - Math.pow(randLat,2));
-	randLong = (Math.random() * 2 - 1) * maxLong;
+	var maxDeg = radtoDeg(radius / EARTH_RADIUS);
+	var randLat = (Math.random() * 2 - 1) * maxDeg;
+	var maxLong = Math.sqrt(Math.pow(maxDeg,2) - Math.pow(randLat,2));
+	var randLong = (Math.random() * 2 - 1) * maxLong;
 	
 	finLat = initLat + randLat;
 	finLong = initLong + randLong;
@@ -14,14 +16,35 @@ function getPoint(initLat, initLong, radius) {
 	console.log("Longitude of the destination is " + finLong);}
 
 function go() {
+	saveVars();
 	console.log("Go function triggered");
-	console.log("Current distance is: " + distance);
+	console.log("Current radius is: " + radius);
+	window.location.replace('interface2.html');
 }
 
-function setDist(value) {
-	distance = value;
-	console.log("setDist function with input " + value + " triggered");
-	console.log("Current distance is: " + distance);
+function setRadius(value) {
+	radius = value;
+	console.log("setRadius function with input " + value + " triggered");
+	console.log("Current radius is: " + radius);
+}
+
+function loadVars() {
+	if (true) { // fix this later
+		initLat = sessionStorage.getItem("initLat");
+		initLong = sessionStorage.getItem("initLong");
+		finLat = sessionStorage.getItem("finLat");
+		finLong = sessionStorage.getItem("finLong");
+	}
+}
+
+function saveVars() {
+	if (true) { // fix this later
+		sessionStorage.setItem("initLat", initLat);
+		sessionStorage.setItem("initLong", initLong);
+		sessionStorage.setItem("finLat", finLat);
+		sessionStorage.setItem("finLong", finLong);
+		console.log("saveVars successfully triggered");
+	}
 }
 
 function radtoDeg(value) {
